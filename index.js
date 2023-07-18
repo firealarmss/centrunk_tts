e9962de23652ccd538ee6fc3d349a96780028da2
firealarmss <ko4uyj@gmail.com>
2023-07-17 23:10:16 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const cron = require('node-cron');
 const say = require('say');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function playWords(text) {
     say.speak(text, '', 1, (error) => {
         if (error) {
-            console.error('Error while playing :', error);
+            console.error('Error while playing:', error);
         }
     });
 }
@@ -17,13 +23,33 @@ function getCurrentTime() {
 }
 
 function hourlyMessage() {
-    const message = `Attention Attention This is the, CTRS repeater, The current time is ${getCurrentTime()}.`;
+    const message = `Attention. Attention. This is the CTRS repeater. The current time is ${getCurrentTime()}.`;
     playWords(message);
     console.log('Played Hourly message');
-
 }
+
+function interactiveMode() {
+    rl.question('Press any key to call the function. Press "q" to quit.\n', (answer) => {
+        if (answer.toLowerCase() === 'q') {
+            rl.close();
+            return;
+        }
+        if (answer.toLowerCase() === 'n'){
+            const message1 = `Attention. Attention. Nathan is gay, The current time is ${getCurrentTime()}.`;
+            playWords(message1);
+            console.log('Played nate message');
+        }
+
+        console.log('Calling the function...');
+        interactiveMode();
+    });
+}
+
 hourlyMessage();
 
 cron.schedule('0 * * * *', () => {
     hourlyMessage();
 });
+
+// Enter interactive mode
+interactiveMode();
